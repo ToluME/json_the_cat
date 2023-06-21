f682db633504e940692173a0795650e4509409b1
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-21 00:19:42 -0600

    Code refractor to functional

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,23 @@
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Request error:', error);
-    return;
-  }
+    const data = JSON.parse(body);
 
-  const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, `Breed '${breedName}' not found.`);
+    } else {
+      const breed = data[0];
+      callback(null, `Description of '${breedName}': ${breed.description}`);
+    }
+  });
+};
 
-  if (data.length === 0) {
-    console.log(`Breed '${breedName}' not found.`);
-  } else {
-    const breed = data[0];
-    console.log(`Description of '${breedName}': ${breed.description}`);
-  }
-});
+module.exports = { fetchBreedDescription };
