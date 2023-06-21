7533db2b63db8ad30fa68eba669d74ca73174c70
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-21 00:04:03 -0600

    Searching the Siberian breed

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const breedName = process.argv[2];
+
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Request error:', error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+
+  if (data.length === 0) {
+    console.log(`Breed '${breedName}' not found.`);
+  } else {
+    const breed = data[0];
+    console.log(`Description of '${breedName}': ${breed.description}`);
+  }
+});
